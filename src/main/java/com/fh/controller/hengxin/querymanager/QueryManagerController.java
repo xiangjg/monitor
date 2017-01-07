2f2d4356067c928e0d47cca86cc4b193aa9554ed
xiangjingguang@sina.com <xiangjingguang@sina.com>
2017-01-07 09:29:20 +0800

    更新

---
+++ src/main/java/com/fh/controller/hengxin/querymanager/QueryManagerController.java
@@ -0,0 +1,263 @@
+package com.fh.controller.hengxin.querymanager;
+
+import java.io.PrintWriter;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Resource;
+
+import com.fh.service.hengxin.areacode.AreaCodeService;
+import com.fh.service.system.user.UserService;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.session.Session;
+import org.apache.shiro.subject.Subject;
+
+import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.fh.controller.base.BaseController;
+import com.fh.entity.Page;
+import com.fh.util.AppUtil;
+import com.fh.util.ObjectExcelView;
+import com.fh.util.Const;
+import com.fh.util.PageData;
+import com.fh.util.Tools;
+import com.fh.util.Jurisdiction;
+import com.fh.service.hengxin.querymanager.QueryManagerService;
+
+/** 
+ * 类名称：QueryManagerController
+ * 创建人：FH 
+ * 创建时间：2017-01-07
+ */
+@Controller
+@RequestMapping(value="/querymanager")
+public class QueryManagerController extends BaseController {
+	
+	String menuUrl = "querymanager/list.do"; //菜单地址(权限用)
+	@Resource(name="querymanagerService")
+	private QueryManagerService querymanagerService;
+	@Resource(name="areacodeService")
+	private AreaCodeService areacodeService;
+	@Resource(name = "userService")
+	private UserService userService;
+	
+	/**
+	 * 新增
+	 */
+	@RequestMapping(value="/save")
+	public ModelAndView save() throws Exception{
+		logBefore(logger, "新增QueryManager");
+		if(!Jurisdiction.buttonJurisdiction(menuUrl, "add")){return null;} //校验权限
+		ModelAndView mv = this.getModelAndView();
+		PageData pd = new PageData();
+		pd = this.getPageData();
+		pd.put("QUERYMANAGER_ID", this.get32UUID());	//主键
+		querymanagerService.save(pd);
+		mv.addObject("msg","success");
+		mv.setViewName("save_result");
+		return mv;
+	}
+	
+	/**
+	 * 删除
+	 */
+	@RequestMapping(value="/delete")
+	public void delete(PrintWriter out){
+		logBefore(logger, "删除QueryManager");
+		if(!Jurisdiction.buttonJurisdiction(menuUrl, "del")){return;} //校验权限
+		PageData pd = new PageData();
+		try{
+			pd = this.getPageData();
+			querymanagerService.delete(pd);
+			out.write("success");
+			out.close();
+		} catch(Exception e){
+			logger.error(e.toString(), e);
+		}
+		
+	}
+	
+	/**
+	 * 修改
+	 */
+	@RequestMapping(value="/edit")
+	public ModelAndView edit() throws Exception{
+		logBefore(logger, "修改QueryManager");
+		if(!Jurisdiction.buttonJurisdiction(menuUrl, "edit")){return null;} //校验权限
+		ModelAndView mv = this.getModelAndView();
+		PageData pd = new PageData();
+		pd = this.getPageData();
+		querymanagerService.edit(pd);
+		mv.addObject("msg","success");
+		mv.setViewName("save_result");
+		return mv;
+	}
+	
+	/**
+	 * 列表
+	 */
+	@RequestMapping(value="/list")
+	public ModelAndView list(Page page){
+		logBefore(logger, "列表QueryManager");
+		//if(!Jurisdiction.buttonJurisdiction(menuUrl, "cha")){return null;} //校验权限
+		ModelAndView mv = this.getModelAndView();
+		PageData pd = new PageData();
+		try{
+			pd = this.getPageData();
+			page.setPd(pd);
+			List<PageData>	varList = querymanagerService.list(page);	//列出QueryManager列表
+			for (PageData qpd:
+					varList) {
+				PageData user = userService.findByUiId(qpd);
+				qpd.put("NAME",user.get("NAME"));
+			}
+			mv.setViewName("hengxin/querymanager/querymanager_list");
+			mv.addObject("varList", varList);
+			mv.addObject("pd", pd);
+			mv.addObject(Const.SESSION_QX,this.getHC());	//按钮权限
+		} catch(Exception e){
+			logger.error(e.toString(), e);
+		}
+		return mv;
+	}
+	
+	/**
+	 * 去新增页面
+	 */
+	@RequestMapping(value="/goAdd")
+	public ModelAndView goAdd(){
+		logBefore(logger, "去新增QueryManager页面");
+		ModelAndView mv = this.getModelAndView();
+		PageData pd = new PageData();
+		pd = this.getPageData();
+		try {
+			List<PageData> areaList = areacodeService.listAll(null);
+			mv.addObject("areaList", areaList);
+			List<PageData> userList = userService.listAllUser(null);
+			mv.addObject("userList", userList);
+			mv.setViewName("hengxin/querymanager/querymanager_edit");
+			mv.addObject("msg", "save");
+			mv.addObject("pd", pd);
+		} catch (Exception e) {
+			logger.error(e.toString(), e);
+		}						
+		return mv;
+	}	
+	
+	/**
+	 * 去修改页面
+	 */
+	@RequestMapping(value="/goEdit")
+	public ModelAndView goEdit(){
+		logBefore(logger, "去修改QueryManager页面");
+		ModelAndView mv = this.getModelAndView();
+		PageData pd = new PageData();
+		pd = this.getPageData();
+		try {
+			List<PageData> areaList = areacodeService.listAll(null);
+			mv.addObject("areaList", areaList);
+			pd = querymanagerService.findById(pd);	//根据ID读取
+			PageData user = userService.findByUiId(pd);
+			pd.put("NAME",user.get("NAME"));
+			List<PageData> userList = new ArrayList<>();
+			userList.add(pd);
+			mv.addObject("userList", userList);
+			mv.setViewName("hengxin/querymanager/querymanager_edit");
+			mv.addObject("msg", "edit");
+			mv.addObject("pd", pd);
+		} catch (Exception e) {
+			logger.error(e.toString(), e);
+		}						
+		return mv;
+	}	
+	
+	/**
+	 * 批量删除
+	 */
+	@RequestMapping(value="/deleteAll")
+	@ResponseBody
+	public Object deleteAll() {
+		logBefore(logger, "批量删除QueryManager");
+		if(!Jurisdiction.buttonJurisdiction(menuUrl, "dell")){return null;} //校验权限
+		PageData pd = new PageData();		
+		Map<String,Object> map = new HashMap<String,Object>();
+		try {
+			pd = this.getPageData();
+			List<PageData> pdList = new ArrayList<PageData>();
+			String DATA_IDS = pd.getString("DATA_IDS");
+			if(null != DATA_IDS && !"".equals(DATA_IDS)){
+				String ArrayDATA_IDS[] = DATA_IDS.split(",");
+				querymanagerService.deleteAll(ArrayDATA_IDS);
+				pd.put("msg", "ok");
+			}else{
+				pd.put("msg", "no");
+			}
+			pdList.add(pd);
+			map.put("list", pdList);
+		} catch (Exception e) {
+			logger.error(e.toString(), e);
+		} finally {
+			logAfter(logger);
+		}
+		return AppUtil.returnObject(pd, map);
+	}
+	
+	/*
+	 * 导出到excel
+	 * @return
+	 */
+	@RequestMapping(value="/excel")
+	public ModelAndView exportExcel(){
+		logBefore(logger, "导出QueryManager到excel");
+		if(!Jurisdiction.buttonJurisdiction(menuUrl, "cha")){return null;}
+		ModelAndView mv = new ModelAndView();
+		PageData pd = new PageData();
+		pd = this.getPageData();
+		try{
+			Map<String,Object> dataMap = new HashMap<String,Object>();
+			List<String> titles = new ArrayList<String>();
+			titles.add("用户ID");	//1
+			titles.add("地区名称");	//2
+			dataMap.put("titles", titles);
+			List<PageData> varOList = querymanagerService.listAll(pd);
+			List<PageData> varList = new ArrayList<PageData>();
+			for(int i=0;i<varOList.size();i++){
+				PageData vpd = new PageData();
+				vpd.put("var1", varOList.get(i).getString("USER_ID"));	//1
+				vpd.put("var2", varOList.get(i).getString("AREA_NAME"));	//2
+				varList.add(vpd);
+			}
+			dataMap.put("varList", varList);
+			ObjectExcelView erv = new ObjectExcelView();
+			mv = new ModelAndView(erv,dataMap);
+		} catch(Exception e){
+			logger.error(e.toString(), e);
+		}
+		return mv;
+	}
+	
+	/* ===============================权限================================== */
+	public Map<String, String> getHC(){
+		Subject currentUser = SecurityUtils.getSubject();  //shiro管理的session
+		Session session = currentUser.getSession();
+		return (Map<String, String>)session.getAttribute(Const.SESSION_QX);
+	}
+	/* ===============================权限================================== */
+	
+	@InitBinder
+	public void initBinder(WebDataBinder binder){
+		DateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+		binder.registerCustomEditor(Date.class, new CustomDateEditor(format,true));
+	}
+}
